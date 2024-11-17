b0fe5b97583718deb0c582a6ac813fdd3f13d209
Prince Asante <princeaffumasante@gmail.com>
2024-11-17 14:33:12 +0000

    change db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,9 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # SQLALCHEMY_DATABASE_UL = "sqlite:///./todosapp.db"
-# POSTGRES_DATABASE_UL = "postgresql://jade:FZ0AuD7buU2qSpekpDKs7OCySIReKs9N@todoapplicationdatabase_x1sk/TodoApplicationDatabase"
+POSTGRES_DATABASE_UL = "postgresql://jade:FZ0AuD7buU2qSpekpDKs7OCySIReKs9N@todoapplicationdatabase_x1sk/TodoApplicationDatabase"
 
-POSTGRES_DATABASE_UL = "postgresql://jade:FZ0AuD7buU2qSpekpDKs7OCySIReKs9N@dpg-cssvhd23esus739qug70-a.frankfurt-postgres.render.com/todoapplicationdatabase_x1sk"
 
 engine = create_engine(POSTGRES_DATABASE_UL)
 
